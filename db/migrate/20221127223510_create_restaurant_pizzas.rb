66832dc97a76c1076f6bec21432816f4be42f55b
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-11-28 02:01:38 +0300

    migrated and added some seed data

--- db/migrate/20221127223510_create_restaurant_pizzas.rb
+++ db/migrate/20221127223510_create_restaurant_pizzas.rb
@@ -1,6 +1,9 @@
 class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
+      t.integer :price
+      t.integer :pizza_id
+      t.integer :restaurant_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!([
+  {
+    name: "gallitos",
+    address: "moi avenue"
+  },
+  {
+    name: "inti",
+    address: "inti"
+  },
+  {
+    name: "kfc",
+    address: "banda street"
+  }
+])
+
+
+Pizza.create!([
+  {
+    name: "Cheese",
+    ingredients: "cheese"
+  },
+  {
+    name: "Pepperoni",
+    ingredients: "whatever"
+  },
+  {
+    name: "code",
+    ingredients: "code"
+  }
+
+])
+
+Restaurant.all.each do |restaurant|
+  rand(1..5).times do
+    # get a random pizza
+    pizza = Pizza.find(Pizza.pluck(:id).sample)
+
+    RestaurantPizza.create!(restaurant_id: restaurant.id, pizza_id: pizza.id, price: rand(5..25))
+  end
+end
+
