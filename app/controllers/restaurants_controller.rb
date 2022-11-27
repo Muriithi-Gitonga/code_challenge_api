d91c51f17eede0c85b85e233fcd81677eb19c9e7
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-11-28 02:33:18 +0300

    added show and added to restaurant serializer

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,7 +6,16 @@ class RestaurantsController < ApplicationController
         render json: restaurant
     end
 
+    def show
+        restaurant = find_restaurant
+        render json: restaurant
+    end
+
     private
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+    end
 
     def render_not_found_response
       render json: { error: "Restaurant not found" }, status: :not_found
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,5 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
+
+  has_many :pizzas
 end
